3c845fa96ce49472558987d8f48f6d24f389b425
AmadouOuryBah <amadourybah3732@gmail.com>
2024-04-25 16:11:21 +0300

    feat add table of orders

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -41,7 +41,6 @@ const Order = ({order}) => {
                           
                             <Td>{order.orderDate}</Td>
                             <Td>{order.totalCost}</Td>
-
                     
                     </Tr>
         
